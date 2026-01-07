a84d85c1a0ff1cff8c4905dc537f83a11ed95c83
Joana Soares <81482172+joanasoares0@users.noreply.github.com>
2026-01-07 20:11:11 +0000

    terraform setup

--- pipeline/main.tf
+++ pipeline/main.tf
@@ -9,6 +9,28 @@ terraform {
 
 provider "google" {
   # Configuration options
-  project = "my-project-id"
-  region  = "us-central1"
+  project = "dtc-de-course-483520"
+  region  = "europe-west1"
+}
+
+resource "google_storage_bucket" "demo-bucket" {
+  # Globally unique bucket name
+  name          = "dtc-de-course-483520-terra-bucket"
+
+  # Region where the bucket will be created
+  # europe-west1 = Belgium (closest stable region to Portugal)
+  location      = "europe-west1"
+
+  # Allows Terraform to delete the bucket even if it contains objects
+  force_destroy = true
+
+  # Automatically delete objects older than 1 day
+  lifecycle_rule {
+    condition {
+      age = 1
+    }
+    action {
+      type = "AbortIncompleteMultipartUpload"
+    }
+  }
 }
